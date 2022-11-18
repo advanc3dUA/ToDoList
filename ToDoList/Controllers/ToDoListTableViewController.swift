fc7b288b953cb6670cf69b5c95fe18caf80e4dcf
advanc3dUA <fash1k@gmail.com>
2022-11-18 13:19:32 +0100

    Saves new items to db w/sqlite

--- ToDoList/Controllers/ToDoListTableViewController.swift
+++ ToDoList/Controllers/ToDoListTableViewController.swift
@@ -7,33 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListTableViewController: UITableViewController {
     
     var itemList = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appending(path: "Items.plist")
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appending(path: "Items.plist"))
         
-        loadItems()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //loadItems()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemList.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemList[indexPath.row]
@@ -44,7 +40,7 @@ class ToDoListTableViewController: UITableViewController {
         cell.accessoryType = item.done ? .checkmark : .none
         
         cell.contentConfiguration = content
-
+        
         return cell
     }
     
@@ -58,20 +54,26 @@ class ToDoListTableViewController: UITableViewController {
     }
     
     // MARK: - Add new item section
-        
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add new task", message: .none, preferredStyle: .alert)
         let addAction = UIAlertAction(title: "Add", style: .default) { action in
-            if let newTask = textField.text {
-                let newItem = Item(title: newTask)
-                self.itemList.append(newItem)
-                
-                self.saveItems()
-                
-                self.tableView.reloadData()
-            }
+            
+            let context = self.getContext()
+            guard let entity = NSEntityDescription.entity(forEntityName: "Item", in: context) else { return }
+            //guard let entity = NSEntityDescription.entity(forEntityName: newTask, in: context) else { return }
+            let newItem = Item(entity: entity, insertInto: context)
+            newItem.title = textField.text
+            newItem.done = false
+            
+            self.itemList.append(newItem)
+            
+            self.saveItems()
+            
+            self.tableView.reloadData()
+            
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         
@@ -88,70 +90,27 @@ class ToDoListTableViewController: UITableViewController {
     
     //MARK: - Model manipulation methods
     
+    fileprivate func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
     fileprivate func saveItems() {
-        let encoder = PropertyListEncoder()
+        let context = getContext()
         do {
-            let data = try encoder.encode(itemList)
-            try data.write(to: dataFilePath!)
-            
+            try context.save()
         } catch {
-            print("accured while encoding: \(error.localizedDescription)")
+            print("error saving to db: \(error.localizedDescription)")
         }
     }
     
-    fileprivate func loadItems() {
-        let decoder = PropertyListDecoder()
-        do {
-            let plistData = try Data(contentsOf: dataFilePath!)
-            itemList = try decoder.decode([Item].self, from: plistData)
-        } catch {
-            print("accured while decoding: \(error.localizedDescription)")
-        }
-    }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    fileprivate func loadItems() {
+//        let decoder = PropertyListDecoder()
+//        do {
+//            let plistData = try Data(contentsOf: dataFilePath!)
+//            itemList = try decoder.decode([Item].self, from: plistData)
+//        } catch {
+//            print("accured while decoding: \(error.localizedDescription)")
+//        }
+//    }
 }
