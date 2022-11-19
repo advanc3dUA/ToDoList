2289a7ffff33b2ff1a8cc1149dd48932fd40fa0d
advanc3dUA <fash1k@gmail.com>
2022-11-19 14:02:12 +0100

    Add categoryVC Add ToDoDB category table and relationships with items table

---
+++ ToDoList/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryTableViewController.swift
+//  ToDoList
+//
+//  Created by Yuriy Gudimov on 19.11.2022.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+}
